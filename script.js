d4f462e15451163e8be613663c5cba3804bfc4c3
Emmett C <60626358+emmettcrass@users.noreply.github.com>
2021-12-01 23:15:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+$('.banana').click(function () {
+  let phonesound = $('audio#phonesound')[0]
+  phonesound.play()
+  $('.banana').toggleClass('jiggle')
+
+})
+
+// $(window).on('cup-load', function () {
+//   $('.load-wrapper').fadeOut('slow');
+// })//
+$(".teacup").click(function () {
+  $("#teacup-spill").toggleClass('flip');
+})
+
+$('.break').click(function () {
+  $('.break').remove()
+  $('.break-menu').css('display', 'block')
+
+})
+$('.lunch').click(function () {
+  $('.lunch').remove()
+  $('.lunch-menu').css('display', 'block')
+})
+
+$('.dropdown').mouseover(function () {
+  $('.dropdown-content').css('display', 'block')
+})
+$('.dropdown').mouseout(function () {
+  $('.dropdown-content').css('display', 'none')
+})
+
+function click(element) {
+  elmeent.click('flip');
+}
+function hover(element) {
+  element.setAttribute('src', 'images/breakfast-post.svg');
+}
+
+function unhover(element) {
+  element.setAttribute('src', 'images/breakfast-pre.svg');
+}
+
+function hoverL(element) {
+  element.setAttribute('src', 'images/lunch-post.svg');
+}
+
+function unhoverL(element) {
+  element.setAttribute('src', 'images/lunch-pre.svg');
+}
+
+hover();
+unhover();
+hoverL();
+unhoverL();
